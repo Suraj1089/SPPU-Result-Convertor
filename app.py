aefae50e88b71aeae1c06b8ebc4896300c7d85b8
Suraj Pisal <surajpisal113@gmail.com>
2022-09-23 09:37:36 +0530

    UI created for displaying text and pdf file in model.py file

--- app.py
+++ app.py
@@ -1,16 +1,59 @@
 import streamlit as st 
 import pandas as pd 
+import numpy as np 
+import seaborn as sns 
+import matplotlib.pyplot as plt 
+import plotly.express as px #interactive plotting
+import re       #clean string
+import PyPDF2  #convert pdf to text 
 
 
-st.title("pdf to csv convertor")
+#set page title and icon
+st.set_page_config(
+    page_title='Result Analysis',
+    page_icon='📃'
+)
+st.title("Result Analysis")
+st.markdown("""
+    ## :outbox_tray: Text data extractor: PDF to Text
+    [![Twitter](https://img.shields.io/twitter/url?label=Twitter&style=social&url=https%3A%2F%2Ftwitter.com%2Fnainia_ayoub)](https://www.twitter.com/nainia_ayoub)
+    [![Linkedin](https://img.shields.io/twitter/url?label=Linkedin&logo=linkedin&style=social&url=https%3A%2F%2Fwww.linkedin.com%2Fin%2Fayoub-nainia%2F%3Flocale%3Den_US)](https://www.linkedin.com/in/ayoub-nainia/?locale=en_US)
+    [![GitHub](https://img.shields.io/twitter/url?label=Github&logo=GitHub&style=social&url=https%3A%2F%2Fgithub.com%2Fnainiayoub)](https://github.com/nainiayoub)
+    Before extracting information from a document, we have to extract text data first. 
+    Hence, this PDF text data extractor was created.
+""")
+#multiple tabls in header
+tab1, tab2 = st.tabs(["📈 PDF TO Excel Convertor", "🗃 Excel data Analyser"])
 
-department = st.selectbox("CHOOSE DEPARTMENT :",options=['IT','CS','E&TC','MECHANICAL','CIVIL'])
-year = st.selectbox("CHOOSE YEAR :",options=['FE','SE','TE','BE'])
 
-pdf_file = st.file_uploader("")
+#add values in tab1
+with tab1:
+    st.write("Drag or Drop pdf file to Convert")
+    department = st.sidebar.selectbox("CHOOSE DEPARTMENT :",options=['IT','CS','E&TC','MECHANICAL','CIVIL'])
+    year = st.sidebar.selectbox("CHOOSE YEAR :",options=['FE','SE','TE','BE'])
 
-button = st.button('CONVERT')
+    pdf_file = st.sidebar.file_uploader("",type='pdf')
 
-if button:
+    button = st.sidebar.button('PREVIEW')
+    file_name = None 
+    if button:
+        #check file is selected or not
+        if pdf_file is None:
+            st.write("Please select a file")
+        else:
+            st.write("pdf is selected")
+            file_name = st.write(pdf_file.name)
+            if 'pdf_file' not in st.session_state:
+                st.session_state['pdf_file'] = pdf_file
+            
+            
+        st.button("DOWNLOAD")
+
+
+
+with tab2:
     d = pd.read_csv('s12.csv')
     st.dataframe(d)
+    st.table(d.columns)
+    st.write("tab2 is pressed")
+
--- model.py
+++ model.py
@@ -2,22 +2,77 @@ import streamlit as st
 import tabula
 import pandas as pd
 import numpy as np 
+import PyPDF2 
+from io import StringIO
+import base64
 
-st.title('pdf to excel converter')
+def displayPDF(file):
+  # Opening file from file path
+  # with open(file, "rb") as f:
+  base64_pdf = base64.b64encode(file.read()).decode('utf-8')
+
+  # Embedding PDF in HTML
+  pdf_display = F'<iframe src="data:application/pdf;base64,{base64_pdf}" width="700" height="1000" type="application/pdf"></iframe>'
+  # Displaying File
+  st.markdown(pdf_display, unsafe_allow_html=True)
+
+@st.cache
+def pdfToText(path):
+    pdfreader = PyPDF2.PdfFileReader(path)
+    no_of_pages = pdfreader.numPages
+    with open('final_txt.txt','w') as f:
+        for i in range(0,no_of_pages):
+            pagObj = pdfreader.getPage(i)
+            f.write(pagObj.extractText())
+    with open('final_txt.txt','r') as f:
+        text = f.read()
+        return text 
+        
+@st.cache(suppress_st_warning=True)
+def showTextFile(path):
+    text_file = F'<iframe src="data:application/pdf;base64,{path}" width="700" height="1000" type="application/pdf"></iframe>'
+  # Displaying File
+    st.markdown(text_file, unsafe_allow_html=True)
+
+# @st.cache
+# def convert_pdf_to_txt_file(path):
+#     texts = []
+#     rsrcmgr = PDFResourceManager()
+#     retstr = StringIO()
+#     laparams = LAParams()
+#     device = TextConverter(rsrcmgr, retstr, laparams=laparams)
+#     # fp = open(path, 'rb')
+#     interpreter = PDFPageInterpreter(rsrcmgr, device)
+    
+#     file_pages = PDFPage.get_pages(path)
+#     nbPages = len(list(file_pages))
+#     for page in PDFPage.get_pages(path):
+#       interpreter.process_page(page)
+#       t = retstr.getvalue()
+#     # text = retstr.getvalue()
+
+#     # fp.close()
+#     device.close()
+#     retstr.close()
+#     return t, nbPages
+
+st.title('pdf to exel')
 
 
-file_name = st.sidebar.text_input('file input: ')
-pdf_file = st.sidebar.file_uploader('upload file :')
+with st.sidebar:
+    st.title("PDF to Text")
+    textOutput = st.selectbox(
+        "How do you want your output data?",
+        ('One text file (.txt)', 'Text file per page (ZIP)'))
 
-if file_name:
-    if pdf_file is not None:
-        df = tabula.read_pdf('{0}.pdf'.format(file_name),pages='all')
-        tabula.convert_into('{0}.pdf'.format(file_name),'data1.csv',output_format='csv',pages='all')
-        data = pd.DataFrame(df[0])
+pdf_file = st.file_uploader("Load your PDF file", type="pdf")
+if pdf_file:
+    # display document
+    with st.expander("Display document"):
+        displayPDF(pdf_file)
 
-        st.write("your data is ...................")
-        st.dataframe(data)
-        data.to_excel('outputsam.xlsx')
-
-else:
-    st.write('please input file name and pdf file')
+show_text = st.button('show text')
+if show_text:
+   p = st.write(pdfToText(pdf_file))
+   showTextFile(p)
+    
