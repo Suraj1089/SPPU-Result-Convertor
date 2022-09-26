9ff09c1d497a823f981a85ff3dd2fb00de318fee
Suraj Pisal <surajpisal113@gmail.com>
2022-09-26 23:58:45 +0530

    temporary graphs added

--- app.py
+++ app.py
@@ -6,6 +6,8 @@ import matplotlib.pyplot as plt
 import plotly.express as px #interactive plotting
 import re       #clean string
 import PyPDF2  #convert pdf to text 
+import time 
+import plotly.express as px 
 
 
 #set page title and icon
@@ -13,47 +15,47 @@ st.set_page_config(
     page_title='Result Analysis',
     page_icon='📃'
 )
-st.title("Result Analysis")
+# st.title("SPPU Result Analyser")
 st.markdown("""
-    ## :outbox_tray: Text data extractor: PDF to Text
-    [![Twitter](https://img.shields.io/twitter/url?label=Twitter&style=social&url=https%3A%2F%2Ftwitter.com%2Fnainia_ayoub)](https://www.twitter.com/nainia_ayoub)
-    [![Linkedin](https://img.shields.io/twitter/url?label=Linkedin&logo=linkedin&style=social&url=https%3A%2F%2Fwww.linkedin.com%2Fin%2Fayoub-nainia%2F%3Flocale%3Den_US)](https://www.linkedin.com/in/ayoub-nainia/?locale=en_US)
-    [![GitHub](https://img.shields.io/twitter/url?label=Github&logo=GitHub&style=social&url=https%3A%2F%2Fgithub.com%2Fnainiayoub)](https://github.com/nainiayoub)
-    Before extracting information from a document, we have to extract text data first. 
-    Hence, this PDF text data extractor was created.
+    ## :outbox_tray: SPPU DATA ANALYSER: PDF to Excel
+    [![GitHub](https://img.shields.io/twitter/url?label=Github&logo=GitHub&style=social&url=https%3A%2F%2Fgithub.com%2Fnainiayoub)](https://github.com/Suraj1089/SPPU-Result-Convertor)
+    
 """)
 #multiple tabls in header
 tab1, tab2 = st.tabs(["📈 PDF TO Excel Convertor", "🗃 Excel data Analyser"])
 
 
-#add values in tab1
 with tab1:
-    st.write("Drag or Drop pdf file to Convert")
-    department = st.sidebar.selectbox("CHOOSE DEPARTMENT :",options=['IT','CS','E&TC','MECHANICAL','CIVIL'])
-    year = st.sidebar.selectbox("CHOOSE YEAR :",options=['FE','SE','TE','BE'])
+    st.sidebar.write('Upload CSV or EXCEL file')
+    uploaded_excel_file = st.sidebar.file_uploader("",type=['csv','xlsx'])
+    if uploaded_excel_file is not None:
+        file_container = st.expander('See uploaded data')
+        excel_file = pd.read_csv(uploaded_excel_file)
+        uploaded_excel_file.seek(0)
+        file_container.write(excel_file)
 
-    pdf_file = st.sidebar.file_uploader("",type='pdf')
+        with st.expander('show pie chart'):
+            with st.spinner("Converting..."):
+                time.sleep(5)
+            st.success("Done!")
 
-    button = st.sidebar.button('PREVIEW')
-    file_name = None 
-    if button:
-        #check file is selected or not
-        if pdf_file is None:
-            st.write("Please select a file")
-        else:
-            st.write("pdf is selected")
-            file_name = st.write(pdf_file.name)
-            if 'pdf_file' not in st.session_state:
-                st.session_state['pdf_file'] = pdf_file
+            labels = list(excel_file['GRD.13'].unique())
+            sizes = list(excel_file['GRD.13'].value_counts())
+            p = px.pie(sizes,labels)
+            st.plotly_chart(p)
             
-            
-        st.button("DOWNLOAD")
+        
+        with st.expander('show bar'):
+            st.plotly_chart(px.bar(excel_file['PTS2.12']))
+
+# if tab2:
+#     with tab2:
+#         st.sidebar.write('Upload CSV or EXCEL file to Analyse')
+#         # excel_file = st.sidebar.file_uploader("",type=['csv','xlsx'])
+#         # if excel_file is not None:
+#         #     df = pd.read_csv(excel_file)
+#         #     st.write(df)
 
 
 
-with tab2:
-    d = pd.read_csv('s12.csv')
-    st.dataframe(d)
-    st.table(d.columns)
-    st.write("tab2 is pressed")
 
