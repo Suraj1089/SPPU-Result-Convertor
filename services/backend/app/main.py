03fb5ea577a1cf2da8e28e59331fb4de4727685a
Suraj Pisal <surajpisal113@gmail.com>
2023-01-24 17:45:48 +0530

    added cors CROS cros origin resource sharing to work with frontend

--- services/backend/app/main.py
+++ services/backend/app/main.py
@@ -1,8 +1,20 @@
 from fastapi import FastAPI, File, UploadFile
+from fastapi.middleware.cors import CORSMiddleware
+import uvicorn
 
 
 app = FastAPI()
 
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
 @app.get("/")
 def home_page():
@@ -13,3 +25,8 @@ def home_page():
 async def upload_file(file: UploadFile = File(...)):
     await file.read()
     return {"filename": file.filename}
+
+
+if __name__ == "__main__":
+    uvicorn.run(app, host="localhost", port=8000)
+
--- services/backend/main.py
+++
@@ -1,9 +0,0 @@
-from fastapi import FastAPI
-
-
-app = FastAPI()
-
-
-@app.get('/')
-def home():
-    return {'hello':'suraj'}
