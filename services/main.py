4c334e71bfcbaf409a7c8d88a179f652eb698883
Suraj Pisal <surajpisal113@gmail.com>
2023-03-24 17:44:07 +0530

    added fastapi backend

---
+++ services/main.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+import uvicorn
+
+app = FastAPI()
+
+templates = Jinja2Templates(directory="templates")
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+@app.get("/", response_class=HTMLResponse)
+def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+if __name__ == "__main__":
+    uvicorn.run('main:app', host="localhost", port=8000, debug=True, reload=True)
+
