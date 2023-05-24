35633eeb4c4414cff249c6a6c67b97a031e749c8
Suraj Pisal <surajpisal113@gmail.com>
2023-05-24 21:10:04 +0530

    Update About_Us.py

--- pages/About_Us.py
+++ pages/About_Us.py
@@ -12,44 +12,44 @@ def app():
 the technologies like Machine Learning, and NLP. We can create scalable applications.""")
 
     st.markdown("""
-    Contact:
-    Name: Suraj Pisal  
-    Email: surajpisal113@gmail.com  
-    LinkedIn: [https://www.linkedin.com/in/surajpisal/](https://www.linkedin.com/in/surajpisal/)  
+    Contact: \n
+    Name: Suraj Pisal  \n
+    Email: surajpisal113@gmail.com  \n
+    LinkedIn: [https://www.linkedin.com/in/surajpisal/](https://www.linkedin.com/in/surajpisal/)  \n
     Portfolio: [https://surajpisal.netlify.app/](https://surajpisal.netlify.app/)  
     """)
 
     st.markdown("""
-    Contact:
-    Name: Vikram Markali  
-    Email: vikrammarkali007@gmail.com  
-    LinkedIn: [https://www.linkedin.com/in/vikram-markali/](https://www.linkedin.com/in/vikram-markali/)  
-    Portfolio: [https://vfolio.netlify.app/](https://vfolio.netlify.app/)  
+    Contact: \n
+    Name: Vikram Markali  \n
+    Email: vikrammarkali007@gmail.com  \n
+    LinkedIn: [https://www.linkedin.com/in/vikram-markali/](https://www.linkedin.com/in/vikram-markali/)  \n
+    Portfolio: [https://vfolio.netlify.app/](https://vfolio.netlify.app/)  \n
     """)
 
     st.markdown("""
     Contact:
-    Name: Durgesh Mahajan  
-    Email: durgeshmahajan1722@gmail.com  
-    LinkedIn: [https://www.linkedin.com/in/durgesh-mahajan-99bab0212/](https://www.linkedin.com/in/durgesh-mahajan-99bab0212/)  
-    Portfolio: [https://durgeshm01722-portfolio.netlify.app/](https://durgeshm01722-portfolio.netlify.app/)  
+    Name: Durgesh Mahajan  \n
+    Email: durgeshmahajan1722@gmail.com  \n
+    LinkedIn: [https://www.linkedin.com/in/durgesh-mahajan-99bab0212/](https://www.linkedin.com/in/durgesh-mahajan-99bab0212/)  \n
+    Portfolio: [https://durgeshm01722-portfolio.netlify.app/](https://durgeshm01722-portfolio.netlify.app/)  \n
     """)
 
     
     st.markdown("""
     Contact:
-    Name: Rasika Ghadge  
-    Email: ghadgerasika16@gmail.com
-    LinkedIn: [https://www.linkedin.com/in/rasika-ghadge-207444217/](https://www.linkedin.com/in/rasika-ghadge-207444217/)  
-    Portfolio: [https://surajpisal.netlify.app/](https://surajpisal.netlify.app/)  
+    Name: Rasika Ghadge  \n
+    Email: ghadgerasika16@gmail.com  \n
+    LinkedIn: [https://www.linkedin.com/in/rasika-ghadge-207444217/](https://www.linkedin.com/in/rasika-ghadge-207444217/)  \n
+    Portfolio: [https://surajpisal.netlify.app/](https://surajpisal.netlify.app/)  \n
     """)
 
     
     st.markdown("""
-    Contact:
-    Name: Pradnya Barve
-    Email: pradnyyabarve@gmail.com
-    LinkedIn: [https://www.linkedin.com/in/pradnya-barve-3a3488218/](https://www.linkedin.com/in/pradnya-barve-3a3488218/)  
+    Contact: \n
+    Name: Pradnya Barve  \n
+    Email: pradnyyabarve@gmail.com  \n
+    LinkedIn: [https://www.linkedin.com/in/pradnya-barve-3a3488218/](https://www.linkedin.com/in/pradnya-barve-3a3488218/)  \n
     """)
 
 
