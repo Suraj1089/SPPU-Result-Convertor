d68e6854a9c9855f3f4049d1a4e246301dfffe2f
Suraj Pisal <surajpisal113@gmail.com>
2023-11-29 08:21:04 +0530

    fix alembic order

--- alembic/versions/a1a8469d9d19_create_user_table.py
+++ alembic/versions/a1a8469d9d19_create_user_table.py
@@ -7,8 +7,8 @@ Create Date: 2023-11-28 22:41:13.839647
 """
 from typing import Sequence, Union
 
-from alembic import op
 import sqlalchemy as sa
+from alembic import op
 
 # revision identifiers, used by Alembic.
 revision: str = 'a1a8469d9d19'
