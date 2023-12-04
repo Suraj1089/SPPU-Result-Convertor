df3407dfc7a0bcb38bfb5f11cede89ce0be7d3a1
Suraj Pisal <surajpisal113@gmail.com>
2023-12-04 18:45:39 +0530

    fix minor bugs

--- internal/config.py
+++ internal/config.py
@@ -2,12 +2,21 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 
 class Settings(BaseSettings):
+    PROJECT_NAME: str = 'SPPU-RESULT-CONVERTER'
     UPLOADCARE_API_KEY: str
     DROPBOX_ACCESS_TOKEN: str
     SECRET_KEY: str
     ALGORITHM: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int
-    EMAILS_ENABLED: bool = False
+    EMAILS_ENABLED: bool = True
+    SMTP_HOST: str
+    SMTP_PORT: int
+    SMTP_LOGIN: str
+    SMTP_PASSWORD: str
+    SMTP_API_KEY: str
+    EMAILS_FROM_NAME: str
+    EMAILS_FROM_EMAIL: str
+    EMAIL_TEMPLATES_DIR: str = "/fastApiProject/internal/email-templates/build"
     model_config = SettingsConfigDict(env_file='.env', env_file_encoding='utf-8')
 
 
--- routers/converter.py
+++ routers/converter.py
@@ -1,26 +1,19 @@
 from typing import Annotated
 
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from fastapi import File, UploadFile
+from utils.user import get_current_active_user
+
 
 router = APIRouter(
     prefix='/files',
-    tags=['files']
+    tags=['files'],
+    dependencies=[Depends(get_current_active_user)]
 )
 
 
 @router.post("/uploadFile/")
 async def create_upload_file(
-    file: Annotated[UploadFile, File(description="A result file read as UploadFile")]
-):
-    return {
-        "filename": file.filename, "file": file.file, "content_type": file.content_type,
-        "size": file.size
-    }
-
-
-@router.get("/uploadFile/")
-async def file_get(
     file: Annotated[UploadFile, File(description="A result file read as UploadFile")]
 ):
     return {
